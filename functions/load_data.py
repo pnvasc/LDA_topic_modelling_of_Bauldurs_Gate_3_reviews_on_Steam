58262ae8bfdbd6091b42387a51dbb6e65cd762b6
Paula <paula@mightanddelight.com>
2024-07-26 15:27:34 +0200

    Update notebook and data loading function

--- functions/load_data.py
+++ functions/load_data.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from config import BG3_data_file_path
 # Load data file
 def load_data(file_path):
-    file_path = BG3_data_file_path
     df = pd.read_csv(file_path)
     return df
